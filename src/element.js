40ae5997b25f2c99e7ffcc569a457aac7ff43314
FND <fnd@localhost.localdomain>
2016-02-14 12:07:24 +0100

    implemented in-place content updates
    form submission is hijax'd, the server response is used to update the
    form's contents
    
    this, of course, is a simplification: normally we'd get back an entire
    HTML document rather than just the form contents, but we'll worry about
    that later

--- src/element.js
+++ src/element.js
@@ -1,10 +1,44 @@
 /*eslint-env browser */
+import http from "./http";
+
 export default class Formix extends HTMLFormElement {
 	attachedCallback() {
+		this.refresh();
 		this.addEventListener("submit", this.onSubmit.bind(this));
 	}
 
 	onSubmit(ev) {
+		let uri = this.getAttribute("action");
+		let method = this.getAttribute("method") || "GET";
+		method = method.toUpperCase();
+		let payload = serialize(this);
+
+		http(method, uri, payload).
+			then(html => {
+				this.innerHTML = html;
+				this.refresh();
+			});
+			// TODO: error handling
+
 		ev.preventDefault();
 	}
+
+	refresh() {
+		let counter = document.createElement("p");
+		counter.textContent = "total: " + this.querySelectorAll("li").length;
+		this.insertBefore(counter, this.firstChild);
+	}
+}
+
+// XXX: too simplistic; edge cases? -- TODO: move into separate library
+function serialize(form) {
+	let fields = form.querySelectorAll("input, select, textarea");
+	return [].reduce.call(fields, (memo, field) => {
+		let name = field.getAttribute("name");
+		if(name) {
+			memo[name] = memo[name] || [];
+			memo[name].push(field.value);
+		}
+		return memo;
+	}, {});
 }
---
+++ src/http.js
@@ -0,0 +1,34 @@
+/*eslint-env browser */
+
+// XXX: DEBUG; mocked for illustration purposes
+
+const DEFAULT_ITEMS = ["foo", "bar", "baz"];
+let userItems = [];
+
+export default function http(method, uri, payload) {
+	userItems.push(payload.name[0]);
+
+	let items = DEFAULT_ITEMS.slice();
+	while(items.length && Math.random() > 0.5) {
+		let i = randomInt(0, items.length - 1);
+		items.splice(i);
+	}
+	items = items.concat(userItems);
+
+	let html = `<input type="text" name="name">
+			<button type="submit">add</button>
+			<ul>` +
+			items.map(item => `<li>${item}</li>`).join("") +
+			"</ul>";
+
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(html);
+		}, randomInt(100, 500));
+	});
+}
+
+// XXX: DEBUG; only required for mock data
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
